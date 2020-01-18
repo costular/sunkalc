86ec9100a352f4e757ede974320789c2290481d1
Diego <costular@gmail.com>
2020-01-18 18:08:09 +0000

    Fix wrong coma

--- src/main/kotlin/com/costular/sunkalc/MathUtils.kt
+++ src/main/kotlin/com/costular/sunkalc/MathUtils.kt
@@ -57,7 +57,7 @@ internal object MathUtils {
     }
 
     fun eclipticLongitude(M: Double): Double {
-        var C = rad * (1.9148 * sin(M) + 0.02 * sin(2 * M) + 0.0003 * sin(3 * M)), // equation of center
+        var C = rad * (1.9148 * sin(M) + 0.02 * sin(2 * M) + 0.0003 * sin(3 * M)) // equation of center
         val P = rad * 102.9372; // perihelion of the Earth
 
         return M + C + P + PI
